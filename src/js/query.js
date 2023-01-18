9eadca4cc69cb1808c244fa99a6fdebb2a696991
jiin-kim109 <jiin.kim109@gmail.com>
2023-01-18 16:05:39 +0900

    add query.js blueprint

--- src/js/query.js
+++ src/js/query.js
@@ -1,29 +1,51 @@
-//import ProgramList from "./models/ProgramList";
+import RmpSearchResultCollection from "./models/RmpSearchResultCollection.js";
 
-/**
- * query.js
- * 
- * get raw HTML for give url string
- * 
- * wrap the raw html into a HTML model and return
- * 
- */
+//const AVAILABLE_PROGRAMS_URL = "https://www.sfu.ca/students/calendar/courses.html";
+const SFU_ACADEMIC_CALANDER = "https://www.sfu.ca/students/calendar/2023/spring/courses.html";
+const RMP_SEARCH_BASE_URL = "https://www.ratemyprofessors.com/search/teachers";
+const RMP_SFU_SID = "U2Nob29sLTE0ODI=";
 
-const AVAILABLE_PROGRAMS_URL = "https://www.sfu.ca/students/calendar/courses.html";
-
-const getSfuProgramList = async () => {
-
-    const response = await fetch("https://www.sfu.ca/students/calendar/courses.html", {
-        headers: {
-            "User-Agent": "SFU_RMP"
-        },
-        redirect: 'follow',
-        mode: 'no-cors',
-    })
-        .then(res => res.redirected ? res.redirect(res.url) : res)
-        .then(res => res.text());
-
-    console.log(response);
+const config = {
+    headers: {
+        "User-Agent": "SFU_RMP"
+    }
 }
 
-getSfuProgramList();
+/**
+ * Return all available course sessions for the current semester
+ * @return Course[]
+ */
+const getAllCourses = async () => {
+
+    const jsdom = require("jsdom");
+
+    const calanderHtml = await fetch(SFU_ACADEMIC_CALANDER, config).then(res => res.text());
+
+    const doc = new jsdom.JSDOM(calanderHtml).window.document;
+
+    const list = doc.querySelectorAll(".course-finder > ul > li > a");
+    list.forEach(a => console.log(a.href));
+}
+
+/**
+ * Return Professor ratings for a given professor name
+ * @return RmpSearchResultCollection
+ */
+const getRmpSearchResultCollection = async (professorName) => {
+
+    const query = "query=" + professorName;
+    const sid = "sid=" + RMP_SFU_SID; 
+    const url = encodeURI(RMP_SEARCH_BASE_URL + "?" + query + "&" + sid);
+
+    const html = await fetch(url, config).then(res => res.text());
+
+    const rmpSearchResultCollection = new RmpSearchResultCollection(html);
+
+    return rmpSearchResultCollection;
+}
+
+export default {
+    getRmpSearchResultCollection
+}
+
+getAllCourses();
