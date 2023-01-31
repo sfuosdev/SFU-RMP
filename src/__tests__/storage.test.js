5ed7c536cbdaae61f7bd9976fa344beeb55c8070
Kevin Park <shp291800@gmail.com>
2023-01-30 17:05:17 -0800

    store and retrieve function for localstorage

--- src/__tests__/storage.test.js
+++ src/__tests__/storage.test.js
@@ -1,9 +1,8 @@
 import { store } from "../js/storage.js"
 import { retreive } from "../js/storage.js"
 
-describe("test storing key,value to local storage and retrieving", () => {
+describe("test storing key,value to local storage and retrieving by key", () => {
 
-  
   test("add then retrieve", () => {
     store("test", "Value")
 	  expect(retreive("test")).toBe("Value");
