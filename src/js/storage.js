2bf63ca3d07ebe558d42fe273c8f4f6fc68e616a
Kevin Park <shp291800@gmail.com>
2023-01-31 16:30:54 -0800

    edit return

--- src/js/storage.js
+++ src/js/storage.js
@@ -2,30 +2,26 @@
 export function store(key, val) {
     var obj = {};
     obj[key] = val
-    chrome.storage.local.set(obj, function() {
-        console.log("Saves to storage",obj);
-    });
+    chrome.storage.local.set(obj);
 
 }
 
 export function retrieve(key) {
-    chrome.storage.local.get(key, function (obj) {
-        console.log("returned js object ", obj);
-    });
+    const data =  await chrome.storage.local.get(key);
+    return data
 }
 
 export function checkStorage(key) {
-   
+   const bool = true
     chrome.storage.local.get(key, function (obj) {
         if(Object.keys(obj).length==0){
-            console.log("false")
-            return false
+            bool = false
         }
         else{
-            console.log("true")
-            return true
+            bool = true
         }
     });
+    return bool
 }
 
 
