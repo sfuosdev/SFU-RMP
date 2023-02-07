e8e3c71271cce0f142adc65b2c9726fe11d7256f
Iain Chun <iainchun@Iains-MacBook-Air.local>
2023-02-06 20:14:35 -0800

    unified css and styled the hover box

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -2,6 +2,6 @@ import goSfu from "./goSfu.js";
 import mySchedule from "./mySchedule.js";
 
 export function render() {
-    goSfu.render();
-    //mySchedule.render();
+    // goSfu.render();
+    mySchedule.render();
 }
