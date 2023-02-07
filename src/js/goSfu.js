49d761c8722c5b4678335b1bf48a0f033fad9910
Iain Chun <iainchun@Iains-MacBook-Air.local>
2023-02-07 02:03:08 -0800

    remove comment from gosfu.js

--- src/js/goSfu.js
+++ src/js/goSfu.js
@@ -1,14 +1,5 @@
 const updateInterval = 5000;
 
-// function highlightClassSearchInstructorNames(node) {
-//     const instructorNames = node.querySelectorAll(`[id^=win0divMTG_INSTR] > span`);
-
-//     instructorNames.forEach(element => {
-//         element.style.fontWeight = "bold";
-//         element.style.color = "blue";
-//     });
-// }
-
 function hoverTooltipOverInstructorNames(node) {
     const instructorNames = node.querySelectorAll(`[id^=win0divMTG_INSTR] > span`);
 
