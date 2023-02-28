dc14a493d9848003dd922dfa870d29a8fad2069f
chaiwonyoun <122844172+chaiwonyoun@users.noreply.github.com>
2023-02-27 19:27:20 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -10,6 +10,31 @@ changeColor.addEventListener("click", async () => {
   wrapper.appendChild(text);
 });
 
+var stat = 1;
+let status1btn = document.getElementById("status1btn");
+let status2btn = document.getElementById("status2btn");
+
+
+// When the button is clicked, inject text element
+status1btn.addEventListener("click", async () => {
+  stat = 1;
+  // document.getElementById("element").style.display = "none";
+  // document.getElementById("element").style.display = "block";
+  console.log("check btn 1")
+  const pElement = document.getElementById("status");
+  pElement.innerText = stat;
+});
+
+// When the button is clicked, inject text element
+status2btn.addEventListener("click", async () => {
+  stat = 2;
+  // document.getElementById("element").style.display = "none";
+  // document.getElementById("element").style.display = "block";
+  console.log("check btn 2")
+  const pElement = document.getElementById("status");
+  pElement.innerText = stat;
+});
+
 /*
 // To access current page, not popup
 changeColor.addEventListener("click", async () => {
