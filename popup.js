9100c0b74f9276cdedcc1a491c7e4efc4202c0cd
Iain Chun <iainchun03@gmail.com>
2023-02-27 17:50:44 -0800

    create popup.html, popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,50 @@
+// export const logMessage = (message) => {
+//   chrome.runtime.sendMessage({
+//       target: "popup", 
+//       message: message
+//   });
+// }
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//       if (request.target === "popup") {
+//           //  show received log message on popup
+//       }
+//   }
+// );
+
 // Query control button
-let changeColor = document.getElementById("changeColor");
+let status_one = document.getElementById("status_one");
+let status_two = document.getElementById("status_two");
+
+const wrapper = document.getElementById("wrapper");
+let timeString = new Date().toLocaleTimeString();
+let dateString = new Date().toDateString();
 
 // When the button is clicked, inject text element
-changeColor.addEventListener("click", async () => {
-  const wrapper = document.getElementById("wrapper");
-  const text = document.createElement("p");
-  text.innerText = "Hello World!";
-  
-  wrapper.appendChild(text);
+status_one.addEventListener("click", async () => {
+  wrapper.innerHTML = "";
+  const divOne = document.createElement("div")
+  divOne.className = "div1";
+  const data = document.createElement("h3");
+  wrapper.appendChild(divOne);
+  divOne.appendChild(data);
+  data.innerText = "Collecting Data...";
+});
+
+status_two.addEventListener("click", async () => {
+  wrapper.innerHTML = "";
+  const divOne = document.createElement("div")
+  divOne.className = "div2";
+  const update = document.createElement("h3");
+  update.innerText = "Last Update:";
+  const time = document.createElement("h2");
+  time.innerText = timeString;
+  const date = document.createElement("p");
+  date.innerText = dateString;
+  wrapper.appendChild(divOne);
+  divOne.appendChild(update);
+  divOne.appendChild(time);
+  divOne.appendChild(date);
 });
 
 /*
